c284b8cc1799f8b312ee5200b0ea4d415a36a277
gabrielknot <gcftutos@gmail.com>
2021-05-11 04:35:02 +0000

    Fixing the image construction

---
+++ kubernetes-setup/roles/kubernetes/files/ipNginxDocker/entryPoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -xe
+: "${MY_POD_NAME?Need an api url}"
+
+sed -i "s/MY_POD_NAME/$MY_POD_NAME/g" /usr/share/nginx/html/index.html
+set -xe
+: "${MY_POD_NAMESPACE?Need an api url}"
+
+sed -i "s/MY_POD_NAMESPACE/$MY_POD_NAMESPACE/g" /usr/share/nginx/html/index.html
+set -xe
+: "${MY_POD_IP?Need an api url}"
+
+sed -i "s/MY_POD_IP/$MY_POD_IP/g" /usr/share/nginx/html/index.html
+
+exec "$@"
