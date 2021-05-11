3f6e93bfc1248e083d53ffbacd047794fa46d697
gabrielknot <gcftutos@gmail.com>
2021-05-11 06:30:54 +0000

    Fixing nginx bug

--- kubernetes-setup/roles/kubernetes/files/ipNginxDocker/entryPoint.sh
+++ kubernetes-setup/roles/kubernetes/files/ipNginxDocker/entryPoint.sh
@@ -2,13 +2,15 @@
 set -xe
 : "${MY_POD_NAME?Need an api url}"
 
-sed -i "s/MY_POD_NAME/$MY_POD_NAME/g" /usr/share/nginx/html/index.html
+set -xe
+: "${MY_POD_IP?Need an api url}"
 set -xe
 : "${MY_POD_NAMESPACE?Need an api url}"
 
 sed -i "s/MY_POD_NAMESPACE/$MY_POD_NAMESPACE/g" /usr/share/nginx/html/index.html
-set -xe
-: "${MY_POD_IP?Need an api url}"
+
+sed -i "s/MY_POD_NAME/$MY_POD_NAME/g" /usr/share/nginx/html/index.html
+
 
 sed -i "s/MY_POD_IP/$MY_POD_IP/g" /usr/share/nginx/html/index.html
 
